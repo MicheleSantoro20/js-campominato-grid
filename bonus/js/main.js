8aba9fbe280a6d10782511b058c323ee95d2c0e5
Michele Santoro <michelesantoro2000@icloud.com>
2023-03-03 17:19:26 +0100

    Push bonus

--- bonus/js/main.js
+++ bonus/js/main.js
@@ -11,46 +11,144 @@ Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro e
 
  let numeroMassimoCaselle = 100;
  
+ let difficoltà = document.getElementById ('difficoltà')
+ if (difficoltà.value == 100) {
+            
+            numeroMassimoCaselle = 100;
+            button.addEventListener ('click', 
+            function ()  {
 
- button.addEventListener ('click', 
+                let i = 0;
+                console.log (difficoltà.value)
+
+                gridDom.innerHTML = '';
+
+
+                while ( i < numeroMassimoCaselle) {
+            
+                let currentSquare = createNewSquare (i + 1);
+        
+                gridDom.append(currentSquare);
+
+
+                currentSquare.addEventListener ('click',
+                    function () {
+                        currentSquare.classList.remove ('square')
+                        currentSquare.classList.add ('square-click')
+                        console.log (currentSquare.innerHTML)
+                    }
+                )
+        
+                i++;
+                
+
+                }
+
+            }
+        
+        );
+
+
+
+
+        function createNewSquare (number) {
+            const currentElement = document.createElement('div');
+            currentElement.classList.add('square');
+            currentElement.innerHTML = number;
+            return currentElement;
+        }
+
+ } else if (difficoltà.value == 81) {
+
+    numeroMassimoCaselle = 81;
+    button.addEventListener ('click', 
     function ()  {
 
         let i = 0;
-
+        console.log (difficoltà.value)
 
         gridDom.innerHTML = '';
 
 
         while ( i < numeroMassimoCaselle) {
-       
-        let currentSquare = createNewSquare (i + 1);
-   
-         gridDom.append(currentSquare);
+    
+        let currentSquare2 = createNewSquare2 (i + 1);
+
+        gridDom.append(currentSquare2);
 
 
-         currentSquare.addEventListener ('click',
+        currentSquare2.addEventListener ('click',
             function () {
-                currentSquare.classList.remove ('square')
-                currentSquare.classList.add ('square-click')
-                console.log (currentSquare.innerHTML)
+                currentSquare2.classList.remove ('square2')
+                currentSquare2.classList.add ('square-click2')
+                console.log (currentSquare2.innerHTML)
             }
-         )
-   
+        )
+
         i++;
         
 
         }
 
     }
- 
- );
+
+);
 
 
 
 
- function createNewSquare (number) {
-    const currentElement = document.createElement('div');
-    currentElement.classList.add('square');
-    currentElement.innerHTML = number;
-    return currentElement;
+function createNewSquare2 (number) {
+    const currentElement2 = document.createElement('div');
+    currentElement2.classList.add('square2');
+    currentElement2.innerHTML = number;
+    return currentElement2;
 }
+
+ } else if (difficoltà.value == 64) {
+    numeroMassimoCaselle = 64;
+    button.addEventListener ('click', 
+    function ()  {
+
+        let i = 0;
+        console.log (difficoltà.value)
+
+        gridDom.innerHTML = '';
+
+
+        while ( i < numeroMassimoCaselle) {
+    
+        let currentSquare3 = createNewSquare3 (i + 1);
+
+        gridDom.append(currentSquare3);
+
+
+        currentSquare3.addEventListener ('click',
+            function () {
+                currentSquare3.classList.remove ('square3')
+                currentSquare3.classList.add ('square-click3')
+                console.log (currentSquare3.innerHTML)
+            }
+        )
+
+        i++;
+        
+
+        }
+
+    }
+
+);
+
+
+
+
+function createNewSquare3 (number) {
+    const currentElement3 = document.createElement('div');
+    currentElement3.classList.add('square3');
+    currentElement3.innerHTML = number;
+    return currentElement3;
+} 
+ };
+
+
+
