502f2aecf00e2dde07ff8ba22e75deb04b918907
Michele Santoro <michelesantoro2000@icloud.com>
2023-03-03 15:44:03 +0100

    Esercizio semi-concluso. Manca il reset nel secondo click del play

---
+++ js/main.js
@@ -0,0 +1,45 @@
+ /* Consegna
+L’utente clicca su un bottone che genererà una griglia di gioco quadrata.
+Ogni cella ha un numero progressivo, da 1 a 100.
+Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+ */
+ 
+ const gridDom = document.getElementById ('grid');
+
+ const button = document.getElementById ('playButton')
+
+ button.addEventListener ('click', 
+    function ()  {
+
+        let onOff = false;
+        let i = 0;
+
+
+        do {
+            let currentSquare = createNewSquare (i + 1);
+   
+            gridDom.append(currentSquare);
+      
+           i++;
+
+        }   while ( i < 100);
+
+        if (i = 100) {
+            
+        }
+        
+
+    }
+ 
+ );
+
+
+
+
+ function createNewSquare (number) {
+    const currentElement = document.createElement('div');
+    currentElement.classList.add('square');
+    currentElement.innerHTML = number;
+    return currentElement;
+}
